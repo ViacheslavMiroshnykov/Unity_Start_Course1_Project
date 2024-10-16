9bffb05b546f4c94310559e25d875f342d097cad
ViacheslavMiroshnykov <viacheslav.miroshnykov@gmail.com>
2024-10-16 19:16:55 +0300

    Edited script

--- Assets/Scripts/TerminalControl.cs
+++ Assets/Scripts/TerminalControl.cs
@@ -7,7 +7,14 @@ public class TerminalControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.WriteLine("Привет! Какой терминал вы хотите взломать сегодня?");
+        ShowMainMenu("Слава");
+    }
+
+    void ShowMainMenu (string PlayerName)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Привет, "+PlayerName+"!");
+        Terminal.WriteLine("Какой терминал хотите взломать сегодня?");
         Terminal.WriteLine(" ");
         Terminal.WriteLine("Введите 1 - городскуя библиотека");
         Terminal.WriteLine("Введите 2 - полицейский участов");
