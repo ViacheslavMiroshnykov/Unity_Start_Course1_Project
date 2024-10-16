e2a4ef6af6315985276c3fddcb85b9d4a1dc9e5d
ViacheslavMiroshnykov <viacheslav.miroshnykov@gmail.com>
2024-10-16 23:23:15 +0300

    Edited script

--- Assets/Scripts/TerminalControl.cs
+++ Assets/Scripts/TerminalControl.cs
@@ -16,15 +16,36 @@ public class TerminalControl : MonoBehaviour
         Terminal.WriteLine("Привет, "+PlayerName+"!");
         Terminal.WriteLine("Какой терминал хотите взломать сегодня?");
         Terminal.WriteLine(" ");
-        Terminal.WriteLine("Введите 1 - городскуя библиотека");
+        Terminal.WriteLine("Введите 1 - городская библиотека");
         Terminal.WriteLine("Введите 2 - полицейский участов");
         Terminal.WriteLine("Введите 3 - космический корабль");
         Terminal.WriteLine("Введите ваш выбор:");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnUserInput(string input) 
     {
-        
+        if (input == "1")
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("Библиотека, серьезно? Попробуй что-то посложнее...");
+            Terminal.WriteLine("Введите ОК чтобы продолжить:");
+        }
+
+        else if (input == "ОК")
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("В этот раз выбери что-то посложнее.");
+            Terminal.WriteLine("Ну что, какой взламываем на этот раз??");
+            Terminal.WriteLine(" ");
+            Terminal.WriteLine("Введите 1 - городская библиотека");
+            Terminal.WriteLine("Введите 2 - полицейский участов");
+            Terminal.WriteLine("Введите 3 - космический корабль");
+            Terminal.WriteLine("Введите ваш выбор:"); 
+        }
+
+        if (input == "2"){
+            Terminal.ClearScreen();
+            Terminal.WriteLine("В этот раз не лучше... Иди-ка ты спать, а...");
+        }
     }
 }
